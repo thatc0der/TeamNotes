2927cc88951636022ce7e98473daf63427697254
Matt Lefebvre <lefebvrem@wit.edu>
2019-11-10 06:18:33 -0500

    Made it so there has to be a file open at all times

--- TeamNotes/src/TeamNotes.java
+++ TeamNotes/src/TeamNotes.java
@@ -55,112 +55,29 @@ public class TeamNotes extends Application {
 	            "    return text;\n" +
 	            "})()";
 	
-//	private Button initializeOpenButton() {
-//		
-//	}
-//	
-//	private Button initialize
-	
-	private void createNewTab(TabPane tabPane) {
-		// Create the Tab and HTMLEditor components
-        final Tab tab = new Tab("Untitled Doc"); 
-        final HTMLEditor editor = new HTMLEditor();
-  
-        // Add the HTMLEditor 
-        tab.setContent(editor); 
-        tabPane.getTabs().add(tab); 
-            
-        documents.put(tab, null);
-        editors.put(tab, editor);
+	// Initializes the Open file Button
+	private Button initializeOpenButton(Stage stage, TabPane tabPane) {
+		final Button open = new Button("Open File");
+		open.setTooltip(new Tooltip("Click to open an HTML file"));
+		open.setOnAction(event -> {
+			// Create the FileChooser and set its filter to only show HTML files
+			final FileChooser fileChooser = new FileChooser();
+			fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML Files", "*.html"));
+			
+			// Get the File the user selected and the contents into a single String then create the tab. Also check if they did not choose a file
+			final File file = fileChooser.showOpenDialog(stage);
+			if (file == null) {
+				return;
+			}
+			
+			createTabFromFile(tabPane, file);
+		});
+		
+		return open;
 	}
 	
-	private void createTabFromFile(TabPane tabPane, File file) {
-        // Create the Tab and the HTMLEditor component
-        final Tab tab = new Tab(file.getName()); 
-        final HTMLEditor editor = new HTMLEditor();
-		final StringBuilder builder = new StringBuilder();
-		try {
-		    final BufferedReader in = new BufferedReader(new FileReader(file));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		    	builder.append(str);
-		    }
-		    in.close();
-		} catch (IOException e) {
-		}
-
-        // Set the HTMLEditors content, add the HTMLEditor to the Tab, add the Tab to the TabPane
-        editor.setHtmlText(builder.toString());
-        tab.setContent(editor); 
-        tabPane.getTabs().add(tab); 
-
-        documents.put(tab, file);
-        editors.put(tab, editor);
-	}
-	
-	private void closeTab(TabPane tabPane, Tab tab) {
-		documents.remove(tab);
-		editors.remove(tab);
-		tabPane.getTabs().remove(tab);
-	}
-	
-	private void closeAndReopenTab(TabPane tabPane, Tab tab, File file) {
-		// Get the index of the Tab from the TabPane's getTabs() List
-		final int index = tabPane.getTabs().indexOf(tab);
-		
-		// Remove the Tab from the TabPane and remove the Tab from the documents/editors Maps
-		tabPane.getTabs().remove(tab);
-		documents.remove(tab);
-		editors.remove(tab);
-		
-		// Create the new Tab and its HTML Editor then set the HTMLEditors text to the contents of the file
-		final Tab newTab = new Tab(file.getName());
-		final HTMLEditor editor = new HTMLEditor();
-		final StringBuilder builder = new StringBuilder();
-		try {
-		    final BufferedReader in = new BufferedReader(new FileReader(file));
-		    String str;
-		    while ((str = in.readLine()) != null) {
-		    	builder.append(str);
-		    }
-		    in.close();
-		} catch (IOException e) {
-		}
-        editor.setHtmlText(builder.toString());
-		
-        // Set the HTMLEditor to the Tab and add the Tab to the TabPane
-        newTab.setContent(editor);
-		tabPane.getTabs().add(index, newTab);
-		
-		// Map the Tab to its File/HTMLEditor
-		
-        documents.put(newTab, file);
-        editors.put(newTab, editor);
-	}
-	
-	private String getSelectedText(Tab tab) {
-        final WebView webView = (WebView) editors.get(tab).lookup("WebView");
-        if (webView == null) {
-        	return null;
-        }
-        
-        final WebEngine engine = webView.getEngine();
-        final Object selection = engine.executeScript(SELECT_TEXT);
-     	return (selection instanceof String) ? (String) selection : null;
-	}
-
-	
-	@Override
-	public void start(Stage stage) {
-		// Create the root VBox pane
-		final VBox root = new VBox();
-		root.setStyle("-fx-background-color: WHITE");
-		
-		// Create the TabPane (and create blank tab so the GUI isnt empty when run) and HBox pane (will be the button bar)
-		final TabPane tabPane = new TabPane();
-		createNewTab(tabPane);
-		final HBox buttonPane = new HBox();
-		
+	// Initializes the Save file Button
+	private Button initializeSaveButton(Stage stage, TabPane tabPane) {
 		// Create the Save, Open, and New Doc buttons and define their listeners
 		final Button save = new Button("Save File");
 		save.setTooltip(new Tooltip("Click to save an existing file or create a new one"));
@@ -198,26 +115,19 @@ public class TeamNotes extends Application {
 			closeAndReopenTab(tabPane, current, file);
 		});
 		
-		final Button open = new Button("Open File");
-		open.setTooltip(new Tooltip("Click to open an HTML file"));
-		open.setOnAction(event -> {
-			// Create the FileChooser and set its filter to only show HTML files
-			final FileChooser fileChooser = new FileChooser();
-			fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("HTML Files", "*.html"));
-			
-			// Get the File the user selected and the contents into a single String then create the tab. Also check if they did not choose a file
-			final File file = fileChooser.showOpenDialog(stage);
-			if (file == null) {
-				return;
-			}
-			
-			createTabFromFile(tabPane, file);
-		});
-		
+		return save;
+	}
+	
+	// Initializes the new document Button
+	private Button initializeDocButton(TabPane tabPane) {
 		final Button newTab = new Button("New Document");
 		newTab.setTooltip(new Tooltip("Click to create a new, blank document"));
 		newTab.setOnAction(event -> createNewTab(tabPane));
-		
+		return newTab;
+	}
+	
+	// Initializes the run code Button
+	private Button initializeCodeButton(TabPane tabPane) {
 		final Button code = new Button("Run Code");
 		code.setTooltip(new Tooltip("Highlight and click this button to execute Java code"));
 		code.setOnAction(event -> {
@@ -256,9 +166,130 @@ public class TeamNotes extends Application {
 			outputStage.show();
         });
 		
+		return code;
+	}
+	
+	// Creates a new Tab (document) on a TabPane
+	private void createNewTab(TabPane tabPane) {
+		// Create the Tab and HTMLEditor components
+        final Tab tab = addCloseListener(new Tab("Untitled Doc"), tabPane); 
+        final HTMLEditor editor = new HTMLEditor();
+  
+        // Add the HTMLEditor 
+        tab.setContent(editor); 
+        tabPane.getTabs().add(tab); 
+            
+        documents.put(tab, null);
+        editors.put(tab, editor);
+	}
+	
+	// Creates a new Tab (document) and fills the document with the File's contents
+	private void createTabFromFile(TabPane tabPane, File file) {
+        // Create the Tab and the HTMLEditor component
+        final Tab tab = addCloseListener(new Tab(file.getName()), tabPane); 
+        final HTMLEditor editor = new HTMLEditor();
+		final StringBuilder builder = new StringBuilder();
+		try {
+		    final BufferedReader in = new BufferedReader(new FileReader(file));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		    	builder.append(str);
+		    }
+		    in.close();
+		} catch (IOException e) {
+		}
+
+        // Set the HTMLEditors content, add the HTMLEditor to the Tab, add the Tab to the TabPane
+        editor.setHtmlText(builder.toString());
+        tab.setContent(editor); 
+        tabPane.getTabs().add(tab); 
+
+        documents.put(tab, file);
+        editors.put(tab, editor);
+	}
+	
+	// Adds a close listener to a tab that when closed checks if its the last remaining file. If so it opens a new doc so there is always a doc open
+	private Tab addCloseListener(Tab tab, TabPane tabPane) {
+		tab.setOnClosed(event -> {
+			if (tabPane.getTabs().size() == 0) {
+				createNewTab(tabPane);
+			}
+		});
+		return tab;
+	}
+	
+	// Closes a Tab and removes it from the internal maps
+	private void closeTab(TabPane tabPane, Tab tab) {
+		documents.remove(tab);
+		editors.remove(tab);
+		tabPane.getTabs().remove(tab);
+	}
+	
+	// Closes, saves, and reopens a Tab 
+	private void closeAndReopenTab(TabPane tabPane, Tab tab, File file) {
+		// Get the index of the Tab from the TabPane's getTabs() List
+		final int index = tabPane.getTabs().indexOf(tab);
 		
-		// Add the Buttons to the HBox and add the two panes to the root pane
-		buttonPane.getChildren().addAll(open, save, newTab, code);
+		// Remove the Tab from the TabPane and remove the Tab from the documents/editors Maps
+		tabPane.getTabs().remove(tab);
+		documents.remove(tab);
+		editors.remove(tab);
+		
+		// Create the new Tab and its HTML Editor then set the HTMLEditors text to the contents of the file
+		final Tab newTab = addCloseListener(new Tab(file.getName()), tabPane);
+		final HTMLEditor editor = new HTMLEditor();
+		final StringBuilder builder = new StringBuilder();
+		try {
+		    final BufferedReader in = new BufferedReader(new FileReader(file));
+		    String str;
+		    while ((str = in.readLine()) != null) {
+		    	builder.append(str);
+		    }
+		    in.close();
+		} catch (IOException e) {
+		}
+        editor.setHtmlText(builder.toString());
+		
+        // Set the HTMLEditor to the Tab and add the Tab to the TabPane
+        newTab.setContent(editor);
+		tabPane.getTabs().add(index, newTab);
+		
+		// Map the Tab to its File/HTMLEditor
+		
+        documents.put(newTab, file);
+        editors.put(newTab, editor);
+	}
+	
+	// Method to get the selected (highlighted) text in a HTMLEditor
+	private String getSelectedText(Tab tab) {
+        final WebView webView = (WebView) editors.get(tab).lookup("WebView");
+        if (webView == null) {
+        	return null;
+        }
+        
+        final WebEngine engine = webView.getEngine();
+        final Object selection = engine.executeScript(SELECT_TEXT);
+     	return (selection instanceof String) ? (String) selection : null;
+	}
+
+	
+	@Override
+	public void start(Stage stage) {
+		// Create the TabPane (and create blank tab so the GUI isnt empty when run) and HBox pane (will be the button bar)
+		final TabPane tabPane = new TabPane();
+		createNewTab(tabPane);
+		
+		// Create the HBox which will act as the button bar, create all the buttons, and add all the buttons to the bar (HBox)
+		final HBox buttonPane = new HBox();
+		final Button open = initializeOpenButton(stage, tabPane);
+		final Button save = initializeSaveButton(stage, tabPane);
+		final Button doc = initializeDocButton(tabPane);		
+		final Button code = initializeCodeButton(tabPane);
+		buttonPane.getChildren().addAll(open, save, doc, code);
+		
+		// Create the root VBox pane, set the background color to white and add the button bar/tab pane to the root pane
+		final VBox root = new VBox();
+		root.setStyle("-fx-background-color: WHITE");
         root.getChildren().add(buttonPane);
         root.getChildren().add(tabPane);
         
